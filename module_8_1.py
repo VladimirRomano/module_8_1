c8660a3e612be8d976f600ef88ee751810cd34ae
Владимир Романов <romanidze85@gmail.com>
2024-10-30 02:32:32 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,16 @@
+def add_everything_up(a, b):
+    try:
+        a + b
+    except TypeError:
+        a = str(a)
+        b = str(b)
+        return a + b
+    else:
+        a = float(a)
+        b = float(b)
+        return round(a + b, 3)
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
