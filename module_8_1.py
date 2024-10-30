77e524af6f3160c10b1b86b11f611e37345005e6
Владимир Романов <romanidze85@gmail.com>
2024-10-30 16:13:01 +0300

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -1,16 +1,14 @@
 def add_everything_up(a, b):
     try:
         a + b
+        a = float(a)
+        b = float(b)
+        return round(a + b, 3)
     except TypeError:
         a = str(a)
         b = str(b)
         return a + b
-    else:
-        a = float(a)
-        b = float(b)
-        return round(a + b, 3)
-
-
+        
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
